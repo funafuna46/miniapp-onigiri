29a68934deebb7c1dc08cdf78c369db3584c7309
funafuna46 <orbystana@yahoo.co.jp>
2023-10-26 12:16:51 +0900

    注文内容の表示、メッセージ一定時間経過後の削除

--- script.js
+++ script.js
@@ -1,25 +1,68 @@
+const onigiriData = [
+  { src: 'images/onigiri.png', label: 'しゃけ' },
+  { src: 'images/onigiri.png', label: 'うめ' },
+  { src: 'images/onigiri.png', label: 'おかか' },
+  { src: 'images/onigiri.png', label: 'ツナマヨ' },
+  { src: 'images/onigiri.png', label: 'たらこ' },
+  { src: 'images/onigiri.png', label: 'こんぶ' },
+  { src: 'images/onigiri.png', label: 'わかめ' },
+  { src: 'images/onigiri.png', label: '天むす' },
+  { src: 'images/onigiri.png', label: 'ゆかり' },
+  // 必要に応じて他のおにぎりのデータも追加
+];
+
 document.getElementById("start").addEventListener("click", startGame);
 
-function startGame() {
-    document.getElementById("instructions").style.display = "none";
-    document.getElementById("game").style.display = "block";
-    // ゲームの初期化処理をここに追加
+let currentOrderCount = 3;  // 初めの注文数
+
+function generateOrder(count) {
+  const orderCounts = {};
+
+  for (let i = 0; i < count; i++) {
+      const randomIndex = Math.floor(Math.random() * onigiriData.length);
+      const label = onigiriData[randomIndex].label;
+
+      if (!orderCounts[label]) {
+          orderCounts[label] = 0;
+      }
+      orderCounts[label]++;
+  }
+
+  const orders = [];
+  for (const [label, count] of Object.entries(orderCounts)) {
+      orders.push(`${label}を${count}個`);
+  }
+
+  return orders;
 }
+
+
+function startGame() {
+  document.getElementById("instructions").style.display = "none";
+  document.getElementById("game").style.display = "block";
+
+  // 注文を生成
+  const orders = generateOrder(currentOrderCount);
+  const orderDiv = document.getElementById("order");
+
+  // 1秒後に「いらっしゃいませ！」メッセージを消し、注文を表示
+  setTimeout(() => {
+      document.getElementById("greeting").textContent = "";
+      orderDiv.textContent = orders.join(", ") + "ください。";
+
+      // さらに5秒後に注文内容を消す
+      setTimeout(() => {
+          orderDiv.textContent = "";
+      }, 5000);
+  }, 1000);  // 1秒後に実行
+
+  currentOrderCount++;  // 次の注文のために注文数を増やす
+}
+
+
 
 // 他のゲームの機能やロジックをこの下に追加する
 document.addEventListener('DOMContentLoaded', function() {
-  const onigiriData = [
-      { src: 'images/onigiri.png', label: 'しゃけ' },
-      { src: 'images/onigiri.png', label: 'うめ' },
-      { src: 'images/onigiri.png', label: 'おかか' },
-      { src: 'images/onigiri.png', label: 'ツナマヨ' },
-      { src: 'images/onigiri.png', label: 'たらこ' },
-      { src: 'images/onigiri.png', label: 'こんぶ' },
-      { src: 'images/onigiri.png', label: 'わかめ' },
-      { src: 'images/onigiri.png', label: '天むす' },
-      { src: 'images/onigiri.png', label: 'ゆかり' },
-      // 必要に応じて他のおにぎりのデータも追加
-  ];
 
   const onigirisContainer = document.getElementById('onigiris');
 
